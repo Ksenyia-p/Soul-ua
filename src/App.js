7d3f5ad12a4cc204db2fc4a2ecb37475db114e6b
Ksenyia-p <kseniapetrashko16@gmail.com>
2025-04-16 18:28:35 +0300

    Менюшка не в ту папку пішла, зробила нормально

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Main from './pages/main/Main';
-import Menu from './components/bestsellers/menu/Menu';
+import Menu from './components/menu/Menu';
 import Wishlist from './pages/wishlist/wishlist';
 import ShoppingBag from './components/shopping bag/ShoppingBag';
 import PersonalData from "./pages/personal data/PersonalData";
