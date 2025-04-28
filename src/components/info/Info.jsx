4b49c81707c7f2f450be571c8a5b1288f4a44132
anastasiia <nsty06@icloud.com>
2025-04-28 15:43:41 +0300

    зробила компонент з текстом(Я ГЕНІЙ)

---
+++ src/components/info/Info.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Info.module.css';
+
+const Info = () => {
+    return (
+        <>
+            <div className={styles.info}>
+                <h1>SOUL — ЦЕ УКРАЇНСЬКИЙ БРЕНД СПОРТИВНОГО ОДЯГУ</h1>
+                <h2>Основна мета, створювати якісний одяг для активних людей, що підкреслює індивідуальність</h2>
+            </div>
+        </>
+    )
+};
+
+export default Info;
