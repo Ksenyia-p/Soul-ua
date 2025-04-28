816acd69ec4bed08a7ab66e9711c6b4d11995a88
anastasiia <nsty06@icloud.com>
2025-04-28 20:07:44 +0300

    створила сторінку асортименту(Я ГЕНІЙ)

---
+++ src/pages/assortment/Assortment.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+const Assortment = () => {
+    return (
+        <div>
+            <Header />
+            Assortment
+            <Footer/>
+        </div>
+    );
+};
+
+export default Assortment;
