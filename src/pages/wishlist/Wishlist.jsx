8271973fd4e302b5e367736f1945e5e4fe970feb
anastasiia <nsty06@icloud.com>
2025-04-29 21:09:51 +0300

    додала дефолтний шлях до вішлісту(Я ГЕНІЙ)

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import FilterAndSort from "../../components/filter and sort icons/FilterAndSort";
-import backIcon from "../../icons/arrow for way.svg";
+import Way from "../../components/way/Way";
 import styles from "./Wishlist.module.css";
 
 const Wishlist = () => {
-    const navigate = useNavigate();
-
     return (
         <div>
             <Header/>
-
-            <div className={styles.wishlist}>
-                <div className={styles.wishlistContainer}>
-                    <h3 onClick={() => navigate(-1)} className={styles.backButton}>
-                        Головна
-                    </h3>
-                    <div className={styles.backIcon}>
-                        <img src={backIcon} alt="/"/>
-                    </div>
-                    <div className={styles.text}>
-                        <h3>Вішліст</h3>
-                    </div>
-                </div>
-            </div>
+            <Way>Вішліст</Way>
             <FilterAndSort/>
             <Footer/>
         </div>
