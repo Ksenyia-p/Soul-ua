16d68f1ce40bb75717195ca1f66ada6e45b49a02
anastasiia <nsty06@icloud.com>
2025-04-29 21:09:37 +0300

    створила дефолтний шлях(Я ГЕНІЙ)

---
+++ src/components/way/Way.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Way.module.css';
+import {useNavigate} from "react-router-dom";
+
+import backIcon from "../../icons/arrow for way.svg";
+
+
+const Way = ({children, onClick}) => {
+    const navigate = useNavigate();
+
+    return (
+        <div className={styles.wishlist}>
+            <div className={styles.wishlistContainer}>
+                <h3 onClick={() => navigate(-1)} className={styles.backButton}>
+                    Назад
+                </h3>
+                <div className={styles.backIcon}>
+                    <img src={backIcon} alt="/"/>
+                </div>
+                <div className={styles.text}>
+                    <h3>{children}</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Way;
+
