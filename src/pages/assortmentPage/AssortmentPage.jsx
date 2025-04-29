50d00a278373954c1f563066d663fea707b37642
Ksenyia-p <kseniapetrashko16@gmail.com>
2025-04-29 11:29:33 +0300

    Шляхи для меню

---
+++ src/pages/assortmentPage/AssortmentPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+
+const AssortmentPage = () => {
+    return (
+
+        <div>
+            <Header />
+            Page
+            <Footer />
+        </div>
+    );
+};
+
+export default AssortmentPage;
