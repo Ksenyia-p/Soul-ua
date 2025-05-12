5cc56044cc8ab9d3fc6051b1c38112d691547ce2
Maks Kuso <maks@MacBook-Air-Maks.local>
2025-05-13 00:05:58 +0300

    )

--- src/components/inputField/InputField.jsx
+++ src/components/inputField/InputField.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import clsx from 'clsx';
 import inputStyles from './inputStyles.module.css';
 
-const InputField = ( { label, type, name, id, children, wrapperClass } ) => {
+const InputField = ( { onChange, label, type, name, id, children, wrapperClass, animationClass } ) => {
     return (
-        <div className={clsx(inputStyles.wrapper, wrapperClass)}>
+        <div className={clsx(inputStyles.wrapper, wrapperClass, animationClass)}>
             <label htmlFor={id}><h3>{label}</h3></label>
-            <input type={type} name={name} id={id} />
+            <input type={type} name={name} id={id} onChange={onChange} />
             {children}
         </div>
     );
