6996454d651c3c24d9627a3cbae560f88ab0d4c2
Maks Kuso <maks@MacBook-Air-Maks.local>
2025-05-12 20:18:30 +0300

    добавив прікол

--- src/pages/personal data/PersonalData.jsx
+++ src/pages/personal data/PersonalData.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import Way from "../../components/way/Way";
+import stylesPersonal from "./PersonalData.module.css";
 
 const PersonalData = () => {
     return (
         <div>
             <Header />
-            PersonalData
+                <Way>Особистий кабінет</Way>
+
             <Footer/>
         </div>
     );
