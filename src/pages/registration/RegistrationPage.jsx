8945e46b0e8d6905f4c4e8a87fed28613a318391
Maks Kuso <maks@MacBook-Air-Maks.local>
2025-05-13 00:07:32 +0300

    сторінка реєстрації (поки пуста)

---
+++ src/pages/registration/RegistrationPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../../components/header/Header";
+import Way from "../../components/way/Way";
+
+const RegistrationPage = () => {
+    return (
+        <div>
+            <Header />
+            <Way>Реєстрація</Way>
+        </div>
+    );
+};
+
+export default RegistrationPage;
