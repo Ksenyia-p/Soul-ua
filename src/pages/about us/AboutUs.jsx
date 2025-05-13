d015d718d0ee98ebac99eccc5eb6d967f9ffeeab
Solomiia1604 <solomiiavasiv@gmail.com>
2025-05-14 01:34:47 +0300

    About us

--- src/pages/about us/AboutUs.jsx
+++ src/pages/about us/AboutUs.jsx
@@ -1,15 +1,171 @@
-import React from 'react';
+import React from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import styles from "./AboutUs.module.css";
 
 const AboutUs = () => {
-    return (
-        <div>
-            <Header />
-            About Us
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Header />
+      <main className={styles.content}>
+        <h1 className={styles.title}>Про бренд</h1>
+        <h4 className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur. Erat et gravida aliquam odio
+          ornare elit ut.. Proin pharetra nunc in malesuada eleifend iaculis..
+          In congue et bibendum mauris facilisi vestibulum phasellus.. Ipsum
+          aliquet arcu porttitor imperdiet a aliquet.. Viverra sem varius eu
+          congue donec ac adipiscing et ac.. Sit et tellus id pharetra. Bibendum
+          vel elementum pellentesque egestas pharetra.. Amet porttitor ut enim
+          sit neque scelerisque facilisis.. Sapien vitae cursus aliquet egestas
+          amet sed.. Leo pellentesque sed ornare sapien magna volutpat.. Aliquam
+          cursus eget gravida ac id commodo.. Ut quam eu non eget donec..
+          Facilisis viverra bibendum in elementum morbi non urna justo.. Viverra
+          sit sit elit aenean facilisis consequat sit malesuada.. Consectetur
+          vel sit blandit risus pellentesque.. Scelerisque pretium imperdiet
+          augue arcu rhoncus tortor sed ultrices integer.. In id purus urna ac
+          in vitae.. Euismod varius praesent nulla nullam dui.. Lobortis felis
+          sem pharetra porttitor. Non purus eget arcu nisi turpis et curabitur
+          tristique non.. Non sed eget sit imperdiet purus risus vitae.. Porta
+          orci elit elementum eget urna dictum.. Malesuada magna odio
+          sollicitudin augue nunc in arcu semper aliquam.. Pulvinar lectus eu
+          faucibus volutpat malesuada lobortis.. Diam sed sed imperdiet risus.
+          Sit tortor nunc a tristique. Posuere velit enim ut risus ultricies
+          sit.. Volutpat egestas sociis enim dui eu consectetur turpis semper..
+          Volutpat elit amet metus leo id risus condimentum ultricies.. Massa in
+          non nunc sit arcu viverra bibendum convallis.. Arcu nisl parturient
+          rhoncus ac aliquam nisl morbi.. Sollicitudin varius interdum quam
+          phasellus dui sit eget mollis diam.. Ac diam urna neque amet
+          consequat.. Nibh non at vel convallis semper lacus faucibus eget..
+          Volutpat auctor in varius convallis ut pulvinar.. Feugiat nibh nec
+          pharetra vitae convallis.. In aliquam quis habitasse scelerisque
+          auctor sapien tristique rhoncus.. Commodo arcu cursus facilisi id
+          aliquam commodo.. Felis ipsum odio a amet orci.. Etiam neque proin
+          interdum ultricies urna gravida mi tellus ultricies.. Arcu viverra
+          consequat tortor proin egestas leo porta.. Leo amet euismod phasellus
+          magna malesuada tristique amet mauris consectetur.. Nunc sed sagittis
+          magna posuere tincidunt venenatis.. Mauris nunc etiam at libero
+          pellentesque.. Ut orci diam volutpat et magnis orci tincidunt.. Donec
+          eget nec scelerisque egestas lobortis.. Pharetra pharetra lectus odio
+          lacus nisl accumsan phasellus lorem et.. Odio ut tortor leo a morbi
+          massa ipsum vestibulum.. Tristique elementum aliquet quam pharetra.
+          Nulla consequat bibendum at ultrices odio sollicitudin aliquam a..
+          Tincidunt diam quis quam ultrices purus.. Donec duis fermentum
+          adipiscing adipiscing nunc augue massa molestie.. Arcu suscipit
+          egestas risus pellentesque vitae.. Eu egestas tempus pulvinar
+          tincidunt. Etiam non neque odio tortor sed.. Fusce tempor ullamcorper
+          varius massa consectetur at a rhoncus placerat.. Etiam elementum
+          rhoncus odio est urna dui.. Donec eu quis sit auctor cursus varius
+          fusce.. Nisl hendrerit duis urna et vel.. Arcu neque sit sed quis.
+          Facilisi enim sed bibendum ut id.. Tellus morbi id aliquet congue
+          lectus augue dignissim.. Imperdiet egestas sem sit aliquam enim
+          ullamcorper dictum.. Massa in id et malesuada ut tristique donec a
+          in.. Adipiscing diam mi quis tortor vitae.. Faucibus id tincidunt non
+          elementum. Fermentum curabitur integer ultrices congue. Et viverra
+          lobortis massa egestas tristique turpis volutpat.. Montes faucibus sed
+          scelerisque tempus gravida morbi rhoncus at.. Pretium cras quis
+          ridiculus faucibus at vel pulvinar.. Enim dui porttitor penatibus
+          consectetur sit lacus ipsum.. Vulputate faucibus ut integer commodo
+          leo vitae.. Tempus pellentesque viverra faucibus tellus. Diam ultrices
+          volutpat nulla pellentesque magna.. Proin egestas rhoncus purus sit
+          porta.. Est duis a neque quam congue in morbi justo tristique..
+          Adipiscing mattis a gravida montes pretium in et.. Diam consectetur
+          cursus curabitur viverra sem nunc ligula luctus et.. Cras justo
+          fermentum velit etiam. Mattis nibh pretium posuere blandit amet.. Amet
+          sed enim ullamcorper et mattis sed tortor pellentesque.. Nunc sit
+          feugiat turpis vehicula. Adipiscing vitae euismod lobortis in donec..
+          Neque sed duis malesuada mattis venenatis.. Vitae morbi vulputate
+          purus turpis tincidunt sit enim cras.. Aliquet aliquam aliquam iaculis
+          ultrices sed nunc magna.. Purus feugiat justo magnis risus magna
+          quisque fames dictum commodo.. Donec dictum venenatis eros neque.
+          Pharetra at vitae facilisis imperdiet. Fames vitae a diam cras lacus..
+          Elementum quis enim egestas luctus velit vitae at.. Eu semper velit at
+          vitae felis vel in cursus.. Amet sit consectetur duis nam suspendisse
+          scelerisque.. Ultrices erat enim scelerisque sed risus consequat
+          tellus.. Pretium aliquam id id aenean nunc nisi nec faucibus vivamus..
+          Ornare erat phasellus varius tellus etiam mauris.. Nisl vel ipsum
+          tincidunt egestas tempus arcu lacus in.. Ut volutpat amet scelerisque
+          rhoncus vulputate in nunc.. Sem in purus consectetur placerat neque
+          in.. Velit feugiat nibh phasellus magna montes vitae nullam
+          tristique.. Sed varius pretium placerat elementum sed viverra
+          malesuada.. Aliquet tincidunt eget quam tellus arcu egestas.. Morbi
+          mauris eget vitae vel sit.. Nascetur vel orci enim sapien elementum..
+          Massa lacus quis malesuada cursus suspendisse vulputate eu.. Turpis id
+          feugiat a sit eu lobortis vulputate cursus habitasse.. Sollicitudin
+          adipiscing nibh nisl id fermentum pellentesque nunc.. Amet gravida
+          eget nibh purus habitant ultrices aliquam.. Aliquet sem vel non ut
+          condimentum.. Risus nunc morbi sapien non felis.. Integer ultrices
+          molestie erat erat non dictum.. Velit risus praesent morbi et iaculis
+          pellentesque.. Aliquet id arcu at pretium. Elementum in risus pulvinar
+          sed scelerisque faucibus sed.. Lobortis nisl tempor donec ullamcorper
+          orci turpis placerat quam rhoncus.. Neque sit pretium scelerisque
+          gravida eu et tincidunt massa facilisis.. Enim elit pellentesque massa
+          enim. Praesent pulvinar risus leo consequat. Libero donec tincidunt mi
+          amet habitant.. Ut orci blandit luctus nulla nulla.. Tortor amet
+          ultrices diam nibh ac at.. Mi tellus orci eros orci mauris viverra
+          enim.. Pretium non amet scelerisque vel id velit pellentesque..
+          Tincidunt in etiam egestas purus aliquet sed imperdiet volutpat
+          lectus.. Amet semper ipsum sit sit nunc fermentum ipsum nisi
+          placerat.. Quis pharetra mi in sed congue vel.. Donec dapibus aliquam
+          dui viverra nullam.. Orci urna maecenas ornare massa eget viverra id
+          id scelerisque.. Aliquam amet egestas lorem ipsum potenti lacus
+          senectus.. Odio lectus enim vitae hac ultrices id est.. Varius donec
+          ullamcorper sollicitudin consequat elementum quisque aliquam mattis
+          fringilla.. Felis vestibulum massa mi purus urna urna pretium purus
+          in.. Elementum a euismod rutrum id metus sagittis integer.. Mattis
+          sagittis amet vulputate sed risus erat duis ultrices.. Egestas at urna
+          feugiat tortor mi.. Nunc tempus magna neque leo nisi tellus amet
+          auctor eget.. Pellentesque erat aliquam tempus nec id velit id.. Proin
+          in pellentesque placerat interdum. Convallis faucibus faucibus id
+          bibendum dignissim ut congue sollicitudin.. Eu quis porttitor
+          consequat amet a ultrices.. Nullam nulla dolor tristique arcu et..
+          Pretium proin eget suspendisse viverra. Magna netus orci sit arcu leo
+          amet.. Pellentesque suspendisse iaculis nullam nec eu at suspendisse
+          vitae gravida.. Elementum sed orci aliquet lorem aenean euismod sem
+          pellentesque nunc.. Malesuada sed aliquet leo diam interdum
+          consectetur dui.. Aliquam egestas maecenas eu nisi. Sapien morbi sit
+          nisi consectetur. Bibendum in a ullamcorper nullam praesent tellus
+          habitant aliquet nibh.. Fames nibh congue sit adipiscing tincidunt..
+          Vehicula tortor donec netus scelerisque aliquam enim egestas
+          ullamcorper neque.. Id euismod ac posuere posuere tortor.. Aliquet
+          velit proin eleifend sit vulputate adipiscing nec tincidunt non..
+          Consequat at id vitae a magna mattis rhoncus suspendisse.. In erat
+          orci quam ut faucibus ac.. Commodo amet ut vestibulum curabitur. Dui
+          ultricies ut magnis cursus id mauris sed ante feugiat.. Pharetra in
+          ligula quam odio etiam velit.. Eget eu tortor elit at molestie
+          adipiscing massa.. Sollicitudin morbi ut at vitae. Euismod imperdiet
+          et non aliquam tempus diam.. Tortor nisi nulla amet lacus etiam vitae
+          turpis tincidunt vestibulum.. Sem nisl enim sit amet elementum quam
+          viverra.. Pellentesque massa in leo cursus auctor.. Mauris dolor sed
+          donec faucibus. Leo lacus amet sed ultricies enim sit.. Est est risus
+          sed tellus. Integer adipiscing gravida purus sit augue et sed lectus
+          scelerisque.. Cursus quis sed hac dapibus in penatibus amet a odio..
+          Ut enim morbi aliquet proin id tellus sapien facilisis dolor.. Amet
+          rhoncus mi fringilla posuere et dictum amet purus.. Suspendisse tortor
+          ante suscipit mi eget viverra hendrerit purus.. Viverra sit mauris
+          eros lorem in pretium id cras at.. Sit ipsum consequat a orci donec ac
+          quam in.. At vitae eget amet quisque in.. Iaculis ac adipiscing urna
+          lorem id a posuere vitae.. Duis orci mollis nec ut sed sit viverra..
+          At at amet proin ornare nibh in nisi.. Sed quam aliquet cursus orci.
+          Tristique nisl elementum quis sagittis. Sit neque amet nibh vel non
+          eget quam egestas.. Sociis in tellus augue risus imperdiet.. Aliquam
+          vestibulum pellentesque porta in feugiat pretium blandit.. Lorem odio
+          et urna eget congue ornare a.. Tristique facilisi vulputate dictum
+          vitae ultrices nullam elementum.. Erat aliquet eu blandit nam sed in
+          porttitor elit.. Pretium tortor tortor semper vitae donec.. Lorem
+          vulputate libero convallis semper. Netus nisi ac enim nulla enim
+          malesuada leo.. Et dolor nunc nunc placerat at nisl mattis orci..
+          Rutrum tellus id et mus et viverra et a.. Donec ut nibh rhoncus
+          condimentum amet.. Commodo faucibus ut ornare justo aliquet.. Faucibus
+          tellus et aliquet tristique amet.. Nisl habitant quis est ultrices.
+          Facilisi sed neque tristique arcu sed.. Enim habitant nisl phasellus
+          tortor tincidunt ipsum aliquam sapien consequat.. Potenti sollicitudin
+          in arcu tincidunt. Tincidunt iaculis a fames ipsum vestibulum.. Eget
+          malesuada nulla elementum lacus dapibus proin eleifend mollis..
+        </h4>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default AboutUs;
