b9c3cf07c163b9f84aa95b825bb6d55029e803c9
Maks Kuso <maks@MacBook-Air-Maks-2.local>
2025-05-17 18:53:17 +0300

    Особистий Кабінет (треба буде ще доробити для зміни даних)

--- src/pages/personal data/PersonalData.jsx
+++ src/pages/personal data/PersonalData.jsx
@@ -1,16 +1,88 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../../components/header/Header";
 import Way from "../../components/way/Way";
 import Layout from "../../components/layout/Layout";
 import stylesPersonal from "./PersonalData.module.css";
+import SideBar from "../../components/sideBar/sideBar";
+import clsx from "clsx";
+import Button from "../../components/button/Button";
+import {auth, db} from "../../FirebaseConfigs/FirebaseConfigs";
+import {doc, getDoc} from "firebase/firestore";
+import {onAuthStateChanged} from "firebase/auth";
+import EditPersonalData from "../../components/editPersonalData/editPersonalData";
 
 const PersonalData = () => {
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                try {
+                    const userRef = doc(db, "users", user.uid);
+                    const userSnap = await getDoc(userRef);
+
+                    if (userSnap.exists()) {
+                        setUserData(userSnap.data());
+                    } else {
+                        setError("Користувача не знайдено в базі");
+                    }
+                } catch (err) {
+                    console.error("Помилка отримання користувача:", err);
+                    setError("Помилка при отриманні даних");
+                }
+            } else {
+                setError("Користувач не авторизований");
+            }
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    console.log(userData);
+
     return (
         <div>
-            <Header />
-                <Way>Особистий кабінет</Way>
+            <Header/>
+            <Way>Особистий кабінет</Way>
 
-            <Layout/>
+            <Layout>
+                <SideBar>
+                    <div className={clsx(stylesPersonal.dataWrapper)}>
+                        <h2>Особисті дані</h2>
+
+                        {loading && <h3 className={stylesPersonal.h3Download}>Завантаження...</h3>}
+                        {error && <p style={{color: "red"}}>{error}</p>}
+
+
+                        {!loading && !error && userData && (
+                            isEditing ? (
+                                <EditPersonalData onCancel={() => setIsEditing(false)} />
+                            ) : (
+                                <>
+                                    <div className={clsx(stylesPersonal.listWrapper)}>
+                                        <ul className={clsx(stylesPersonal.list)}>
+                                            <li>
+                                                <div><h3>Прізвище: {userData.lastName}</h3></div>
+                                                <div><h3>Ім'я: {userData.firstName}</h3></div>
+                                            </li>
+                                            <li><h3>Номер телефону: {userData.phone}</h3></li>
+                                            <li><h3>E-mail: {userData.email}</h3></li>
+                                        </ul>
+                                    </div>
+                                    <Button onClick={() => setIsEditing(true)}>
+                                        Змінити
+                                    </Button>
+                                </>
+                            )
+                        )}
+
+                    </div>
+                </SideBar>
+            </Layout>
         </div>
     );
 };
