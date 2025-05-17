8fb6d22a76d760e0cef6da8359d5205cb56b5783
anastasiia <nsty06@icloud.com>
2025-05-17 19:46:46 +0300

    вікно кошик, але ще треба доробити додавання товарів і тд(Я ГЕНІЙ)

--- src/components/shopping bag/ShoppingBag.jsx
+++ src/components/shopping bag/ShoppingBag.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import Header from "../header/Header";
+import React, { useEffect } from 'react';
+import styles from './ShoppingBag.module.css';
+import Cross from '../../icons/cross.svg';
+import Button from "../button/Button";
+import { Link } from "react-router-dom";
 
-const ShoppingBag = () => {
+const ShoppingBag = ({ active, setActive }) => {
+    useEffect(() => {
+        if (active) {
+            document.body.classList.add('bodyLock');
+        } else {
+            document.body.classList.remove('bodyLock');
+        }
+
+        return () => {
+            document.body.classList.remove('bodyLock');
+        };
+    }, [active]);
+
     return (
-        <div>
-            <Header />
-            ShoppingBag
-        </div>
+        <>
+            <div
+                className={`${styles.overlay} ${active ? styles.activeOverlay : ''}`}
+                onClick={() => setActive(false)}
+            />
+
+            <div className={`${styles.shoppingBag} ${active ? styles.active : ''}`}>
+                <div className={styles.upperRectangleContainer}>
+                    <div className={styles.upperRectangle}>
+                        <h2>Кошик</h2>
+                        <button className={styles.closeButton} onClick={() => setActive(false)}>
+                            <img src={Cross} alt="Закрити" />
+                        </button>
+                    </div>
+                </div>
+
+                <div className={styles.bottomRectangleContainer}>
+                    <div className={styles.bottomRectangle}>
+                        <div className={styles.textContainer}>
+                            <div className={styles.text}>
+                                <h3>Підсумок</h3>
+                                <h3>7 позицій</h3>
+                            </div>
+                            <h3>5850 UAH</h3>
+                        </div>
+                        <Link to="/delivery" className={styles.buttonShoppingBag}>
+                            <Button type="button">переглянути та оплатити</Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
